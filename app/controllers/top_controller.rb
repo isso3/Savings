b0fd0ba617c8938cf3f5aa2115b09ef491008830
isso <issoisso3@gmail.com>
2020-04-28 06:39:29 +0900

    try_fix4

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -5,10 +5,8 @@ class TopController < ApplicationController
       # if user != nil
       redirect_to "/#{current_user.id}/beginner"
       # else
-      
+      # redirect_to "/result/#{current_user.id}"
       # end
-    else
-      redirect_to "/result/#{current_user.id}"
     end
   end
 end
