9ac20737b625aa6274065cd0981519879ad1af13
isso <issoisso3@gmail.com>
2020-04-28 06:28:22 +0900

    try_fix

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,12 +1,12 @@
 class TopController < ApplicationController
   def top
     if user_signed_in?
-      user = Saving.find_by(user_id: current_user)
-      if user != nil
-        redirect_to "/result/#{current_user.id}"
-      else
-        redirect_to "/#{current_user.id}/beginner"
-      end
+      # user = Saving.find_by(user_id: current_user)
+      # if user != nil
+      redirect_to "/result/#{current_user.id}"
+      # else
+      #   redirect_to "/#{current_user.id}/beginner"
+      # end
     end
   end
 end
