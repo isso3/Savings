71d8fa3d92fcb6a758c13a08b5ccf11e16bb46f9
isso <issoisso3@gmail.com>
2020-04-28 06:42:18 +0900

    Revert "routes_update"
    This reverts commit 9e032948ef79e648564cadf7594e1611f4bf5f48.

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,7 +3,7 @@ class TopController < ApplicationController
     if user_signed_in?
       user = Saving.find_by(user_id: current_user)
       if user != nil
-        redirect_to "/result/#{current_user.id}"
+        result_path(current_user.id)
       else
         redirect_to "/#{current_user.id}/beginner"
       end
