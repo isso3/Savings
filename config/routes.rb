6c88f9d4fe91c02c2fddd3d6415420678d07eb23
isso <issoisso3@gmail.com>
2020-04-28 07:33:24 +0900

    try_fix5

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root to: 'top#top'
   resources :result, only: [:show, :new, :create, :edit]
+  patch "/result", to: "result#update"
   get "/:id/beginner", to: "result#beginner"
   post "/:id/beginner", to: "result#create_beginner"
-  patch "/result", to: "result#update"
 end
