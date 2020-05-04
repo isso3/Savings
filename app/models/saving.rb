e59a823e48789aeeb8ea16d7cf479ba7ee910b88
isso <issoisso3@gmail.com>
2020-05-05 00:24:39 +0900

    errors_log&calculation_fix

--- app/models/saving.rb
+++ app/models/saving.rb
@@ -1,2 +1,8 @@
 class Saving < ApplicationRecord
+  validates :total_savings,
+            presence: true,
+            on: :create
+  validates :daily_income, :daily_consumption,
+            presence: true,
+            on: :create, on: :update
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200430085821) do
+ActiveRecord::Schema.define(version: 20200504130129) do
 
   create_table "savings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint   "total_savings"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20200430085821) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.integer  "user_id"
+    t.bigint   "evacuation"
     t.index ["user_id"], name: "index_savings_on_user_id", using: :btree
   end
 
